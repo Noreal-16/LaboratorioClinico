7bb4d70ceded0aec7960d2cc80187b20d0c5d38a
Alex_Nole <snole7@gmail.com>
2021-07-24 23:22:34 -0500

    Menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -246,78 +246,51 @@ return [
             'url'  => 'admin/blog',
             'can'  => 'manage-blog',
         ],
+        
+        ['header' => 'Menú Principal'],
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
-        ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
+            'text' => 'Laboratorios',
+            'url'  => '#', 
+            'icon' => 'fas fa-microscope',
         ],
         [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
+            'text' => 'Categorías',
+            'url'  => '#',
+            'icon' => 'fas fa-tasks',
         ],
         [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
+            'text' => 'Exámenes',
+            'url'  => '#',
+            'icon' => 'fas fa-notes-medical',
         ],
+        [
+            'text' => 'Pacientes',
+            'url'  => '#',
+            'icon' => 'fas fa-user-injured',
+        ],
+        [
+            'text' => 'Médicos',
+            'url'  => '#',
+            'icon' => 'fas fa-user-md',
+        ],
+        [
+            'text' => 'Pedidos',
+            'url'  => '#',
+            'icon' => 'fas fa-clipboard-list',
+        ],
+        [
+            'text' => 'Resultados',
+            'url'  => '#',
+            'icon' => 'fas fa-poll',
+        ],
+        
         ['header' => 'labels'],
         [
             'text'       => 'important',
             'icon_color' => 'red',
             'url'        => '#',
         ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+        
     ],
 
     /*
---
+++ resources/views/admin/laboratorios.blade.php
@@ -0,0 +1,5 @@
+@extends('adminlte::page')
+
+@section('content_header')
+    <h1>Laboratorios</h1>
+@stop
